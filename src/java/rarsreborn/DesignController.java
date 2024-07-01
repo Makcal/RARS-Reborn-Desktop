8493f1df13626e88102d92040cb9659bb17e2adc
Damir <damir.sadykov0407@gmail.com>
2024-07-01 23:13:35 +0300

    refactor: adding design controller and application classes

---
+++ src/java/rarsreborn/DesignController.java
@@ -0,0 +1,11 @@
+package rarsreborn;
+
+import javafx.fxml.FXML;
+
+
+public class DesignController {
+    @FXML
+    protected void OnBtnRunAction(){
+        System.out.println("Button 'play' pressed");
+    }
+}
