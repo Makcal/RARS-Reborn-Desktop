48301408b76ddc69effaf21be9ed3e4de3f27b32
Damir <damir.sadykov0407@gmail.com>
2024-07-01 21:42:34 +0300

    refactor: adding fxml design file

--- src/java/rarsreborn/Main.java
+++ src/java/rarsreborn/Main.java
@@ -1,6 +1,8 @@
 package rarsreborn;
 
 public class Main {
+    //  url('file:C:/Users/User/IdeaProjects/RARS/src/resources/rarsreborn/Images/Run.png')
+    //  url('file:../Images/Run.png')
     public static void main(String[] args) {
         Canvas.main(args);
     }
