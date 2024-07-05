04000d372a9134c18b276dc16015e78f435e3e58
satoru gojo <eternalblessed99@gmail.com>
2024-07-05 17:18:35 +0300

    refactor: connect core compilation to frontend

--- src/java/module-info.java
+++ src/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.rarsreborn {
     requires javafx.controls;
     requires javafx.fxml;
+    requires rars.reborn.core;
 
 
     opens rarsreborn to javafx.fxml;
--- src/java/rarsreborn/DesignController.java
+++ src/java/rarsreborn/DesignController.java
@@ -1,11 +1,64 @@
 package rarsreborn;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import rarsreborn.core.Presets;
+import rarsreborn.core.core.register.Register32;
+import rarsreborn.core.exceptions.compilation.CompilationException;
+import rarsreborn.core.exceptions.linking.LinkingException;
+
+import java.util.Iterator;
 
 
 public class DesignController {
     @FXML
-    protected void OnBtnRunAction(){
-        System.out.println("Button 'play' pressed");
+    private Button btn_break;
+    @FXML
+    private Button btn_debug;
+    @FXML
+    private Button btn_run;
+    @FXML
+    private Button btn_save;
+    @FXML
+    private TextArea code_edit_box;
+    @FXML
+    private MenuButton menu_btn;
+    @FXML
+    private MenuItem menu_item_close;
+    @FXML
+    private MenuItem menu_item_close_all;
+    @FXML
+    private MenuItem menu_item_exit;
+    @FXML
+    private MenuItem menu_item_new;
+    @FXML
+    private MenuItem menu_item_open;
+    @FXML
+    private MenuItem menu_item_save;
+    @FXML
+    private MenuItem menu_item_save_as;
+
+    @FXML
+    void OnBtnRunAction(ActionEvent event)  {
+        try {
+            Presets.classical.compile(code_edit_box.getText());
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Presets.classical.run();
+        Iterator<Register32> iterator = Presets.classical.getRegisterFile().getAllRegisters().iterator();
+        System.out.println("===================================");
+        while (iterator.hasNext()){
+            Register32 current = iterator.next();
+            System.out.println("Register: " + current.getName() + ", value: " + current.getValue());
+        }
+        System.out.println("===================================");
     }
 }
